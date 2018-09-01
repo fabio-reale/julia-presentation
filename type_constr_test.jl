1a9fee2b5166a9f8cd468184d90f76b97fb5330a
Fabio Reale <fabio.reale@usp.br>
2018-09-01 19:17:48 -0300

    Implemented a method to create a Normal type from data matrix

--- type_constr_test.jl
+++ type_constr_test.jl
@@ -11,8 +11,23 @@ end
 Normal(μ::Vector{T}, Σ::Matrix{T}) where T = Normal{T}(μ,Σ);
 
 import Base: *
-*(x::Normal{T}, y::Normal{T}) where T = Normal(y.Σ*(x.μ-y.μ), y.Σ*(x.Σ)*y.Σ')
-esp(data::Matrix) = vec(sum(data,dims=1))./length(data)
+*(x::Normal{T}, y::Normal{T}) where T = Normal(y.Σ * (x.μ - y.μ), y.Σ * x.Σ * y.Σ')
 
 # Σ here isn't correct. data'*H*data is
-sample_normal(data::Matrix) = Normal(esp(data), data'*data)
+#sample_normal(data::Matrix) = Normal(esp(data), data'*data)
+esp(data::Matrix, n) = vec(sum(data,dims=1))./n
+esp(data::Matrix) = esp(data, size(data)[1])
+
+function h_matrix(T, n)
+    J = ones(T,n,n)
+    return one(J)-(J/n)
+end
+h_matrix(data::Matrix{T}) where T = h_matrix(T,size(data)[2])
+
+function sample_normal(data::Matrix{T}) where T
+    n, vars = size(data)
+    μ = esp(data, n)
+    H = h_matrix(T, n)
+    Σ = (data'*H*data)/(n-1)
+    return Normal(μ, Σ)
+end
