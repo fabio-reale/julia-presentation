215b083ce8bfbdf5159b8742cb6afa9579ae4069
Fabio Reale <fabio.reale@usp.br>
2018-09-06 18:38:54 -0300

    Renamed file and updated

--- normtype.jl
+++ normtype.jl
@@ -10,8 +10,16 @@ mutable struct Normal{T}
 end
 Normal(μ::Vector{T}, Σ::Matrix{T}) where T = Normal{T}(μ,Σ);
 
-import Base: *
-*(x::Normal{T}, y::Normal{T}) where T = Normal(y.Σ * (x.μ - y.μ), y.Σ * x.Σ * y.Σ')
+function homocedastica(p::Int64, ρ)
+    aux = ones(Float64,p,p)
+    return (1-ρ)*one(aux)+ ρ*aux
+end
+homocedastica(μ, ρ) = homocedastica(length(μ), ρ)
+
+↦(v::Vector, x::Normal) where T = Normal(x.μ+v, x.Σ)
+↦(c::Matrix, x::Normal) where T = Normal(x.μ, c*x.Σ*c')
+↦(y::Normal, x::Normal) where T = y.Σ ↦ y.μ ↦ x
+padronizar(x::Normal) = real.(√x.Σ\one(x.Σ)) ↦ -x.μ ↦ x
 
 # Σ here isn't correct. data'*H*data is
 #sample_normal(data::Matrix) = Normal(esp(data), data'*data)
