e65a219204e7ed51090319df8334aacc01b7fda3
Fabio Reale <fabio.reale@usp.br>
2018-09-09 19:09:22 -0300

    Added method for one(Normal)

--- normtype.jl
+++ normtype.jl
@@ -5,7 +5,7 @@ mutable struct Normal{T}
         if length(μ) == size(Σ)[1] && length(μ) == size(Σ)[2]
             new(μ,Σ)
         else
-            error("Dimension Mismatch!\n")
+            DimensionMismatch("dim(μ) = $(length(μ)), dim(Σ) = $(size(Σ))")
         end
 end
 Normal(μ::Vector{T}, Σ::Matrix{T}) where T = Normal{T}(μ,Σ);
@@ -20,10 +20,15 @@ homocedastica(μ, ρ) = homocedastica(length(μ), ρ)
 ↦(c::Matrix, x::Normal) where T = Normal(x.μ, c*x.Σ*c')
 ↦(y::Normal, x::Normal) where T = y.Σ ↦ y.μ ↦ x
 padronizar(x::Normal) = real.(√x.Σ\one(x.Σ)) ↦ -x.μ ↦ x
+import Base: one
+one(x::Normal) = Normal(zero(x.μ), one(x.Σ))
+
+#This one is crashing
+#one(T::DataType, p::Int) = Normal( zero(Vector{T}(undef,p)), one(Matrix{T}(undef,p,p)) )
 
 # Σ here isn't correct. data'*H*data is
 #sample_normal(data::Matrix) = Normal(esp(data), data'*data)
-esp(data::Matrix, n) = vec∘sum(data,dims=1)./n
+esp(data::Matrix, n) = vec(sum(data,dims=1)./n)
 esp(data::Matrix) = esp(data, size(data)[1])
 
 function h_matrix(T, n)
@@ -36,6 +41,6 @@ function sample_normal(data::Matrix{T}) where T
     n, vars = size(data)
     μ = esp(data, n)
     H = h_matrix(T, n)
-    Σ = (data'*H*data)/(n-1)
-    return Normal(μ, Σ)
+    S = (data'*H*data)/(n-1)
+    return Normal(μ, S)
 end
