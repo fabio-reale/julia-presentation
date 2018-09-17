63ca965fd1615ac43b128088095cab69ddafda14
Fabio Reale <fabio.reale@usp.br>
2018-09-17 17:56:18 -0300

    Added a myexp to handle integer Float exponents

--- myexp.jl
+++ myexp.jl
@@ -6,14 +6,11 @@ Retorna `x*x*...*x`, n vezes. Retorna o mesmo que `x*myexp(x, n-1)`, se n > 0.
 function myexp(x, n::Integer)
     if n < 0
         return error("myexp(x, n) where n ∈ ℕ")
-    end
-    if n == 0
+    elseif n == 0
         return one(x)
-    end
-    if n == 1
+    elseif n == 1
         return x
-    end
-    if n%2 == 1
+    elseif n%2 == 1
         m = n÷2
         y = myexp(x, m)
         return x*y*y
@@ -23,8 +20,4 @@ function myexp(x, n::Integer)
         return y*y
     end
 end
-
-#=
-rotation(θ) = [cos(θ) -sin(θ);
-               sin(θ) cos(θ)]
-=#
+myexp(x, n::Float64) = myexp(x, convert(Int,n))
