3ba027af5ad0331317df8d21b813b0611973fcd2
Fabio Reale <fabio.reale@usp.br>
2018-09-17 18:16:50 -0300

    Removed unnecessary return keyword

--- myexp.jl
+++ myexp.jl
@@ -5,19 +5,19 @@ Retorna `x*x*...*x`, n vezes. Retorna o mesmo que `x*myexp(x, n-1)`, se n > 0.
 """
 function myexp(x, n::Integer)
     if n < 0
-        return error("myexp(x, n) where n ∈ ℕ")
+        error("myexp(x, n) where n ∈ ℕ")
     elseif n == 0
-        return one(x)
+        one(x)
     elseif n == 1
-        return x
+        x
     elseif n%2 == 1
         m = n÷2
         y = myexp(x, m)
-        return x*y*y
+        x*y*y
     else
         m = n÷2
         y = myexp(x, m)
-        return y*y
+        y*y
     end
 end
 myexp(x, n::Float64) = myexp(x, convert(Int,n))
